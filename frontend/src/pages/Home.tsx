226215afb005a5f23feb710f4b2663aae6ff9e08
Vedant Sunil Ghole <131907280+Vedantghole06@users.noreply.github.com>
2025-02-13 00:24:34 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className= "text-blue-500 text-2xl">Hey There!</div>
-    <div className= "text-blue-500 text-2xl">Hey There! I am Vedant </div>
+    <div className= "text-blue-500 text-2xl">Hey There! I am Dalla Bro</div>
   )
 }
 
